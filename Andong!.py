2d2e93b28c1220813e026d9efa4464db93e6cbec
fahrianrama <72341397+fahrianrama@users.noreply.github.com>
2021-01-02 14:34:04 +0700

    Add files via upload

--- Andong!.py
+++ Andong!.py
@@ -12,9 +12,23 @@ conn.execute(
     "CREATE table IF NOT EXISTS Customer (ID_Customer int primary key, nama text, password text, nomor_telepon int)",
 )
 conn.execute(
-   "CREATE table IF NOT EXISTS Pesanan (ID_Pesanan int primary key, user text, driver text, total_biaya int)",
+   "CREATE table IF NOT EXISTS Pesanan (ID_Pesanan int primary key, user text, total_biaya int)",
 )
-
+conn.execute(
+    "CREATE table IF NOT EXISTS Gaji (ID_Driver int, total pesanan int, total_pendapatan int)",
+)
+conn.execute(
+    "insert into Driver values(?,?,?,?)",(12345,'Jono','anu123',823213212)
+)
+conn.execute(
+    "insert into Driver values(?,?,?,?)",(12121,'Joni','anu123',823659236)
+)
+conn.execute(
+    "insert into Pesanan values(?,?,?)",(1111,'Joni',50000)
+)
+conn.execute(
+    "insert into Gaji values(?,?,?)",(12345,13,500000)
+)
 class Login:
     a=1
     def __init__ (self, id_user, nama, telepon, password):
@@ -26,8 +40,27 @@ class Login:
     
     def signin(self):
         self.id_user = int(input('Masukkan ID Anda : '))
-        self.nama = str(input('Masukkan Nama Anda : '))
-        print('Selamat datang ',self.nama)
+        self.password = str(input('Masukkan Password Anda : '))
+        if self.pilihan == 'driver':
+            cek = conn.execute('select * from Driver')
+            for row in cek:
+                if f'{row[0]}' == str(self.id_user):
+                    if f'{row[2]}' == str(self.password):
+                        print('Selamat datang ',f'{row[1]}')
+                    else:
+                        print('Password yang anda masukkan salah')
+                else:
+                    print('Maaf Anda Belum Terdaftar')
+        else:
+            cek = conn.execute('select * from Customer')
+            for row in cek:
+                if f'{row[0]}' == str(self.id_user):
+                    if f'{row[2]}' == str(self.password):
+                        print('Selamat datang ',f'{row[1]}')
+                    else:
+                        print('Password yang anda masukkan salah')
+                else:
+                    print('Maaf Anda Belum Terdaftar')
 
     def signup(self):
         self.id_user = random.randrange(0,1000000)
@@ -40,18 +73,30 @@ class Login:
             conn.execute('insert into Customer values (?,?,?,?)',(self.id_user,self.nama, self.password, self.telepon))
         print('Selamat datang ',self.nama)
 
+    def printer(self):
+        if self.pilihan == 'driver':
+            printer = conn.execute("select * from Driver")
+            for row in printer:
+                print(f'{row[0]}, {row[1]}, {row[2]}, {row[3]}')
+        else:
+            printer = conn.execute("select * from Customer")
+            for row in printer:
+                print(f'{row[0]}, {row[1]}, {row[2]}, {row[3]}')
+
 class Andong:
-    def __init__ (self, driver, customer, jarak):
+    def __init__ (self,id_user, driver, customer, jarak):
         self.driver = driver
         self.customer = customer
         self.jarak = jarak
+        self.id_user = id_user
+
     def ID_Pesanan(self):
         a = random.randrange(0,1000000)
         return a
 
 class Bonceng(Andong):
     def __init__ (self, driver, customer, jarak):
-        super().__init__(driver,customer,jarak)
+        super().__init__(self, driver,customer,jarak)
     
     def biaya(self):
         total = self.jarak * 2500
@@ -59,7 +104,7 @@ class Bonceng(Andong):
     
 class Maem(Andong):
     def __init__ (self, driver, customer, pesanan, harga_pesanan, jarak):
-        super().__init__(driver,customer,jarak)
+        super().__init__(self, driver,customer,jarak)
         self.pesanan = pesanan
         self.harga_pesanan = harga_pesanan
         
@@ -74,7 +119,7 @@ class Maem(Andong):
     
 class Kirim(Andong):
     def __init__ (self, driver, customer, berat_barang, jarak):
-        super().__init__(driver,customer,jarak)
+        super().__init__(self,driver,customer,jarak)
         self.berat_barang = berat_barang
 
     def biaya(self):
@@ -88,17 +133,42 @@ class Kirim(Andong):
 
 pilihan = input('Apakah Anda ingin login sebagai Driver/Customer ? : ')
 print ('Anda masuk sebagai',pilihan)
-pilihan = Login(None,None,None,None)
+masuk = Login(None,None,None,None)
 login = input('Apakah anda ingin daftar/masuk dengan akun yang tersedia? (Daftar/Masuk) : ')
 if login == 'daftar':
-    pilihan.signup()
+    masuk.signup()
 else:
-    pilihan.signin()      
-    
+    masuk.signin()     
 
-printer = conn.execute("select * from Driver")
-for row in printer:
-    print(f'{row[0]}, {row[1]}, {row[2]}, {row[3]}')
+print('Selamat datang di Andong')
+if pilihan == 'driver':
+    fitur=input('Apakah anda ingin mencari pesanan/cek pendapatan ? : ')    
+    if fitur == 'pesanan':
+        pesanan = conn.execute("select * from Pesanan")
+        for row in pesanan:
+            print(f'{row[0]}, {row[1]}, {row[2]}')
+        pilih = int(input('Manakah pesanan yang anda pilih (ID)? : '))
+        if f'{row[0]}' == str(pilih):
+            print('Anda memilih pesanan :',f'{row[0]}','oleh :', f'{row[1]}')
+        else:
+            print('Pesanan yang anda pilih tidak tersedia')
+    else:
+        pendapatan = conn.execute("select * from Gaji")
+        for row in pendapatan:
+            print(f'{row[2]}')
+else:
+    fitur=input('Apakah anda ingin menggunakan fitur Bonceng, Maem, atau Kirim ? :')
+    if fitur == 'bonceng':
+        pesanan = Bonceng(None,None,int(input('Masukkan jarak : ')))
+        print('Total biaya adalah :',pesanan.biaya())
+    elif fitur == 'maem':
+        pesanan = Maem(None,None,str(input('Masukkan nama pesanan : ')),int(input('Masukkan harga pesanan : ')),int(input('Masukkan jarak : ')))
+        print('Total biaya adalah :',pesanan.biaya())
+    else:
+        pesanan = Kirim(None,None,int(input('Masukkan berat barang : ')),int(input('Masukkan jarak : ')))
+        print('Total biaya adalah :',pesanan.biaya())
+# pilihan.printer()
+
 
 # pesanan1= Maem("Jono","Joni","Nasgor", 10000, 5)
 # conn.execute("insert into Pesanan values(?,?,?,?)",(pesanan1.ID_Pesanan(),pesanan1.customer,pesanan1.driver,pesanan1.biaya()))
